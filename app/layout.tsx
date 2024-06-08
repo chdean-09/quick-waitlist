4dc71563f63db88c1f26187a6ebedf202fafe634
jeromecabugwason <jeromecabugwason2@protonmail.com>
2024-06-09 02:06:50 +0800

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { ThemeProvider } from '@/components/theme-provider';
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
           </ThemeProvider>
         </main>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
