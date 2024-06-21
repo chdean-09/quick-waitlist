4bef1bdff7e337d1a020f69011b1c81a00422e6a
chdean-09 <chadandrada20@gmail.com>
2024-06-22 00:39:15 +0800

    added content
    no design yet

---
+++ app/about-us/page.tsx
@@ -0,0 +1,63 @@
+export default function AboutUs() {
+  return (
+    <section className="relative isolate overflow-hidden px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-2xl lg:max-w-4xl">
+        <p className="text-center h-12 font-bold uppercase ">Our Story</p>
+        <figure className="mt-10">
+          <blockquote className="text-center text-base leading-8 sm:text-lg sm:leading-9">
+            <p>
+              It&apos;s been a year since I had the problem and this crazy idea, I&apos;m tired of finding apartments/boarding houses near our school, it&apos;s always unsatisfactory for my taste and it took a long time and energy to find the perfect one. I thought that I was the only one who had this problem. <br />
+
+
+              Turns out I&apos;m not alone, I&apos;ve read a lot of FB group posts and observed that when finding your preferred living space requires a lot of time like months of walking, searching, and waiting for someone to post in the FB groups that one room na maganda and It&apos;s frustrating when the next morning taken na sya agad. Some people want to know the location if it is near your work or school, a unit with aircon, and nice ambience. <br />
+
+
+              So I asked myself, what if gumawa ako ng website? After a year of struggling on core features, identifying the problem, and defining use cases. I found out users need security, convenience, and preference.
+
+
+            </p>
+          </blockquote>
+          <figcaption className="mt-10">
+            {/* <img
+              className="mx-auto h-10 w-10 rounded-full"
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+            <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+              <div className="font-semibold text-gray-900">Judith Black</div>
+              <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
+                <circle cx={1} cy={1} r={1} />
+              </svg>
+              <div className="text-gray-600">CEO of Workcation</div>
+            </div> */}
+          </figcaption>
+        </figure>
+
+        <p className="text-center h-12 font-bold uppercase ">About</p>
+        <figure className="mt-10">
+          <blockquote className="text-center text-base leading-8 sm:text-lg sm:leading-9">
+            <p>
+              Our mission is to revolutionize the rental housing market by providing a seamless, secure, and personalized platform that links tenants with their ideal living spaces. We believe in enhancing the rental experience by streamlining searches, ensuring safety, and prioritizing user preferences. Through innovative technology and a commitment to excellence, we aim to transform the way people find their shelters, making the process safe, fast, and tailored to individual needs. <br />
+
+              Our vision is to redefine the landscape of rental housing by creating a digital ecosystem that seamlessly connects tenants with their ideal space. We envision a nation where finding the perfect living space is effortless, personalized, and safe, transforming the traditional rental process into a pleasing and efficient experience. By leveraging the latest technology and a deep understanding of user needs, we aim to establish ourselves as the leading platform in the rental market, fostering a community of satisfied tenants and property owners alike.
+            </p>
+          </blockquote>
+          <figcaption className="mt-10">
+            {/* <img
+              className="mx-auto h-10 w-10 rounded-full"
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+            <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+              <div className="font-semibold text-gray-900">Judith Black</div>
+              <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
+                <circle cx={1} cy={1} r={1} />
+              </svg>
+              <div className="text-gray-600">CEO of Workcation</div>
+            </div> */}
+          </figcaption>
+        </figure>
+      </div>
+    </section>
+  )
+}
