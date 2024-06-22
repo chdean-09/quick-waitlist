7dbb1714b9fdad9756454ae3ab8d82741feb21b7
chdean-09 <chadandrada20@gmail.com>
2024-06-23 01:19:06 +0800

    about us content
    will polish it more tomorrow

--- app/page.tsx
+++ app/page.tsx
@@ -125,25 +125,51 @@ export default function Home() {
         </div>
       </div>
 
-
-
-      <div className="bg-white">
-        <section className="relative isolate overflow-hidden px-6 py-24 sm:py-32 lg:px-8">
-          <div className="mx-auto max-w-2xl lg:max-w-4xl">
-            <h2 className="text-center text-3xl font-bold mb-16 text-gray-900">About Us</h2>
-            <div className="bg-gray-100 rounded-lg p-8 shadow-lg">
-              <h3 className="text-xl font-semibold mb-4 text-gray-800">Our Mission</h3>
-              <p className="text-gray-800 leading-relaxed mb-8">
-                We aim to revolutionize the rental housing market by providing a seamless, secure, and personalized platform that connects tenants with their ideal living spaces. We believe in enhancing the rental experience by streamlining searches, ensuring safety, and prioritizing user preferences. Through innovative technology and a commitment to excellence, we strive to transform the way people find their shelters, making the process safe, fast, and tailored to individual needs.
-              </p>
-
-              <h3 className="text-xl font-semibold mb-4 text-gray-800">Our Vision</h3>
-              <p className="text-gray-800 leading-relaxed">
-                We envision redefining the landscape of rental housing by creating a digital ecosystem that seamlessly connects tenants with their ideal space. Our goal is to make finding the perfect living space effortless, personalized, and safe, transforming the traditional rental process into a pleasing and efficient experience. By leveraging the latest technology and a deep understanding of user needs, we aim to establish ourselves as the leading platform in the rental market, fostering a community of satisfied tenants and property owners alike.
-              </p>
-            </div>
+      <div className="px-4 py-16 mx-auto w-full md:px-24 lg:px-8 lg:py-20 bg-white">
+        <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+          <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+            <span className="relative inline-block">
+              <svg viewBox="0 0 52 24" fill="currentColor" className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
+                <defs>
+                  <pattern id="db164e35-2a0e-4c0f-ab05-f14edc6d4d30" x="0" y="0" width=".135" height=".30">
+                    <circle cx="1" cy="1" r=".7" />
+                  </pattern>
+                </defs>
+                <rect fill="url(#db164e35-2a0e-4c0f-ab05-f14edc6d4d30)" width="52" height="24" />
+              </svg>
+              <span className="relative">About Us</span>
+            </span>
+          </h2>
+          <p className="text-base text-gray-700 md:text-lg">
+            Revolutionize the rental housing market by providing a seamless, secure, and personalized platform that links tenants with their ideal living spaces.
+          </p>
+        </div>
+        <div className="grid max-w-sm gap-5 mb-8 lg:grid-cols-2 sm:mx-auto lg:max-w-full">
+          <div className="px-10 py-20 text-center border rounded lg:px-5 lg:py-10 xl:py-20">
+            <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase">Mission</p>
+            <a href="/" className="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400" aria-label="Read article" title="Our Mission">
+              Our Mission
+            </a>
+            <p className="max-w-xs mx-auto mb-2 text-gray-700">
+              We believe in enhancing the rental experience by streamlining searches, ensuring safety, and prioritizing user preferences. Through innovative technology and a commitment to excellence, we aim to transform the way people find their shelters, making the process safe, fast, and tailored to individual needs.
+            </p>
+            <a href="/" aria-label="" className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">
+              Read more
+            </a>
           </div>
-        </section>
+          <div className="px-10 py-20 text-center border rounded lg:px-5 lg:py-10 xl:py-20">
+            <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase">Vision</p>
+            <a href="/" className="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400" aria-label="Read article" title="Our Vision">
+              Our Vision
+            </a>
+            <p className="max-w-xs mx-auto mb-2 text-gray-700">
+              We envision a nation where finding the perfect living space is effortless, personalized, and safe, transforming the traditional rental process into a pleasing and efficient experience. By leveraging the latest technology and a deep understanding of user needs, we aim to establish ourselves as the leading platform in the rental market, fostering a community of satisfied tenants and property owners alike.
+            </p>
+            <a href="/" aria-label="" className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">
+              Read more
+            </a>
+          </div>
+        </div>
       </div>
     </>
   );
