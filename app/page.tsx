1cf4aba3630b405aa319340ac84014e1a728a514
chdean-09 <chadandrada20@gmail.com>
2024-06-23 14:38:18 +0800

    about us

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import {
   CardFooter,
   CardHeader,
 } from '@/components/ui/card';
+import Speaker from '/public/speaker.svg';
+import Image from 'next/image';
 
 import { Footer } from '@/components/footer';
 import { Header } from '@/components/header';
@@ -12,12 +14,23 @@ import { Header } from '@/components/header';
 export default function Home() {
   const Title = process.env.NEXT_PUBLIC_HERO_TITLE || 'We are coming Soon';
   const Subtitle = process.env.NEXT_PUBLIC_HERO_SUBTITLE || 'Stay tuned';
+  const siteLogo = process.env.NEXT_PUBLIC_LOGO || Speaker;
   const [firstWord, secondWord, ...restOfWords] = Title.split(' ');
   const animatedTitle = [firstWord, secondWord].join(' ');
   const restOfString = restOfWords.join(' ');
 
   return (
     <>
+      {/* Ga error ang siteLogo idk what to do with it */}
+      {/* <div className="flex justify-center items-center h-20 md:h-28 relative scale-[35%]">
+        <Image
+          src={siteLogo}
+          alt="white square silid"
+          width={500}
+          height={500}
+        />
+      </div> */}
+
       <div className="relative px-4 py-32 mx-auto h-dvh min-h-[800px] sm:min-h-dvh w-full md:px-24 lg:px-8 sm:py-16 lg:py-20 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
         <div className="max-w-2xl mx-auto sm:max-w-xl md:max-w-2xl">
           <div className="text-center">
@@ -121,52 +134,53 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="px-4 py-16 mx-auto w-full md:px-24 lg:px-8 lg:py-20 bg-white">
-        <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-          <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-            <span className="relative inline-block">
-              <svg viewBox="0 0 52 24" fill="currentColor" className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
-                <defs>
-                  <pattern id="db164e35-2a0e-4c0f-ab05-f14edc6d4d30" x="0" y="0" width=".135" height=".30">
-                    <circle cx="1" cy="1" r=".7" />
-                  </pattern>
-                </defs>
-                <rect fill="url(#db164e35-2a0e-4c0f-ab05-f14edc6d4d30)" width="52" height="24" />
-              </svg>
-              <span className="relative">About Us</span>
-            </span>
-          </h2>
-          <p className="text-base text-gray-700 md:text-lg">
-            Revolutionize the rental housing market by providing a seamless, secure, and personalized platform that links tenants with their ideal living spaces.
-          </p>
-        </div>
-        <div className="grid max-w-sm gap-5 mb-8 lg:grid-cols-2 sm:mx-auto lg:max-w-full">
-          <div className="px-10 py-20 text-center border rounded lg:px-5 lg:py-10 xl:py-20">
-            <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase">Mission</p>
-            <a href="/" className="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400" aria-label="Read article" title="Our Mission">
-              Our Mission
-            </a>
-            <p className="max-w-xs mx-auto mb-2 text-gray-700">
-              We believe in enhancing the rental experience by streamlining searches, ensuring safety, and prioritizing user preferences. Through innovative technology and a commitment to excellence, we aim to transform the way people find their shelters, making the process safe, fast, and tailored to individual needs.
-            </p>
-            <a href="/" aria-label="" className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">
-              Read more
-            </a>
+      <section className="text-gray-400 bg-gray-900">
+        <div className="container px-5 py-24 mx-auto">
+          <h1 className="sm:text-3xl text-2xl font-semibold title-font text-center text-white mb-20">About Us
+          </h1>
+          <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+            <div className="p-4 md:w-1/2 flex">
+              <div className="w-12 h-12 p-2 inline-flex items-center justify-center rounded-full bg-gray-800 text-indigo-400 mb-4 flex-shrink-0">
+                <svg fill="#946de8" viewBox="0 0 24 24">
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <g id="mission">
+                      <path d="M21.85,21.15l-7-7a.48.48,0,0,0-.7,0L12.5,15.79,8,11.29V9h3v1a.5.5,0,0,0,.5.5h6a.52.52,0,0,0,.45-.27.51.51,0,0,0-.05-.52L16.12,7.25,17.9,4.79A.51.51,0,0,0,18,4.27.52.52,0,0,0,17.5,4H14V2.5a.5.5,0,0,0-.5-.5h-6a.5.5,0,0,0-.5.5v8.79L2.44,15.85A1.52,1.52,0,0,0,2,16.91V20.5A1.5,1.5,0,0,0,3.5,22h18a.5.5,0,0,0,.46-.31A.47.47,0,0,0,21.85,21.15ZM16.52,5,15.1,7a.48.48,0,0,0,0,.58l1.42,2H12V9h1.5a.5.5,0,0,0,.5-.5V5ZM13,8H8V3h5ZM3.5,21a.5.5,0,0,1-.5-.5V16.91a.47.47,0,0,1,.15-.35L7.5,12.21l4.65,4.64L16.29,21Zm14.21,0-4.5-4.5,1.29-1.29L20.29,21Z"></path>
+                    </g>
+                  </g>
+                </svg>
+              </div>
+              <div className="flex-grow pl-6">
+                <h2 className="text-white text-lg title-font font-medium mb-2">Mission</h2>
+                <p className="leading-relaxed text-base">Revolutionize the rental housing market by providing a seamless, secure, and personalized platform that links tenants with their ideal living spaces. We believe in enhancing the rental experience by streamlining searches, ensuring safety, and prioritizing user preferences. Through innovative technology and a commitment to excellence, we aim to transform the way people find their shelters, making the process safe, fast, and tailored to individual needs.</p>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/2 flex">
+              <div className="w-12 h-12 p-1 inline-flex items-center justify-center rounded-full bg-gray-800 text-indigo-400 mb-4 flex-shrink-0">
+                <svg fill="#946de8" height="200px" width="200px" version="1.1" id="Layer_1" viewBox="0 0 512 512">
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <g>
+                      <g>
+                        <g>
+                          <path d="M505.616,238.008c-1.079-1.279-26.946-31.695-70.603-62.481C394.538,146.983,330.624,112.958,256,112.958 c-74.624,0-138.538,34.025-179.013,62.569c-43.657,30.788-69.523,61.202-70.603,62.482c-8.511,10.09-8.511,25.894,0,35.983 c1.079,1.279,26.946,31.695,70.603,62.481c40.476,28.544,104.389,62.569,179.013,62.569c74.624,0,138.538-34.025,179.013-62.569 c43.657-30.788,69.523-61.202,70.602-62.481C514.128,263.903,514.128,248.099,505.616,238.008z M492.965,263.313 c-0.253,0.299-25.831,30.322-67.865,59.893c-38.44,27.044-98.982,59.282-169.101,59.282c-70.12,0-130.661-32.238-169.101-59.282 c-42.034-29.571-67.611-59.593-67.863-59.891c-3.288-3.896-3.287-10.733-0.002-14.628c0.254-0.299,25.831-30.322,67.865-59.893 c38.439-27.044,98.982-59.282,169.101-59.282c70.12,0,130.661,32.238,169.101,59.282c42.034,29.571,67.611,59.593,67.862,59.89 C496.251,252.582,496.251,259.419,492.965,263.313z"></path>
+                          <path d="M255.997,143.974c-61.771,0-112.025,50.254-112.025,112.026s50.253,112.026,112.025,112.026 c61.771,0,112.025-50.254,112.025-112.026S317.767,143.974,255.997,143.974z M255.997,351.471 c-52.642,0-95.47-42.828-95.47-95.471s42.827-95.471,95.47-95.471c25.691,0,49.04,10.203,66.219,26.767 c-4.909-3.416-10.867-5.424-17.287-5.424c-9.304,0-17.639,4.212-23.21,10.825c-8.13-3.297-16.889-5.024-25.723-5.024 c-37.676,0-68.327,30.651-68.327,68.327s30.652,68.327,68.327,68.327s68.327-30.651,68.327-68.327 c0-6.331-0.871-12.535-2.563-18.537c8.144-5.45,13.518-14.728,13.518-25.241c0-5.205-1.318-10.106-3.637-14.392 c12.423,16.117,19.823,36.294,19.823,58.169C351.466,308.642,308.639,351.471,255.997,351.471z M304.93,242.573 c0.357,0,0.71-0.015,1.063-0.026c1.171,4.359,1.777,8.861,1.777,13.454c0,28.547-23.225,51.773-51.773,51.773 c-28.547,0-51.773-23.225-51.773-51.773s23.225-51.773,51.773-51.773c6.486,0,12.919,1.222,18.909,3.567 c-0.212,1.446-0.327,2.924-0.327,4.429C274.579,228.957,288.194,242.573,304.93,242.573z M304.93,226.018 c-7.608,0-13.796-6.188-13.796-13.796c0-7.607,6.188-13.796,13.796-13.796s13.796,6.188,13.796,13.796 C318.725,219.83,312.536,226.018,304.93,226.018z"></path>
+                        </g>
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+              </div>
+              <div className="flex-grow pl-6">
+                <h2 className="text-white text-lg title-font font-medium mb-2">Vision</h2>
+                <p className="leading-relaxed text-base">Redefine the landscape of rental housing by creating a digital ecosystem that seamlessly connects tenants with their ideal space. We envision a nation where finding the perfect living space is effortless, personalized, and safe, transforming the traditional rental process into a pleasing and efficient experience. By leveraging the latest technology and a deep understanding of user needs, we aim to establish ourselves as the leading platform in the rental market, fostering a community of satisfied tenants and property owners alike.</p>
+              </div>
+            </div>
           </div>
-          <div className="px-10 py-20 text-center border rounded lg:px-5 lg:py-10 xl:py-20">
-            <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase">Vision</p>
-            <a href="/" className="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400" aria-label="Read article" title="Our Vision">
-              Our Vision
-            </a>
-            <p className="max-w-xs mx-auto mb-2 text-gray-700">
-              We envision a nation where finding the perfect living space is effortless, personalized, and safe, transforming the traditional rental process into a pleasing and efficient experience. By leveraging the latest technology and a deep understanding of user needs, we aim to establish ourselves as the leading platform in the rental market, fostering a community of satisfied tenants and property owners alike.
-            </p>
-            <a href="/" aria-label="" className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">
-              Read more
-            </a>
-          </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
