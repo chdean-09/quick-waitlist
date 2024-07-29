5e57e2be2de65605eac704e4f4a88bc6f429026f
chdean-09 <chadandrada20@gmail.com>
2024-07-29 23:16:45 +0800

    updated images and more optimizations to images

--- app/company/about/page.tsx
+++ app/company/about/page.tsx
@@ -11,7 +11,7 @@ export default function AboutUs() {
   return (
     <>
       <PageHero className="mx-auto xl:px-0 flex w-full flex-col items-center justify-center text-center gap-12 mb-10 lg:-mb-16">
-        <PageHeroHeading className="mt-3 text-3xl font-normal leading-snug tracking-tight lg:leading-tight lg:text-4xl w-[95%]">
+        <PageHeroHeading>
           We Revolutionize Rental Housing, suiting to individual needs.
         </PageHeroHeading>
 
@@ -22,7 +22,7 @@ export default function AboutUs() {
 
       <ValueProposition />
 
-    <Team />
+      <Team />
     </>
   );
 }
